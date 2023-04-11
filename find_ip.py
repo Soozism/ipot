fb173d77cdd0c58fc32b6d67c213395550a52f8c
sooz <ali.z.ram.1379@gmail.com>
2023-04-11 13:01:00 +0430

    find open IPs on a port

---
+++ find_ip.py
@@ -0,0 +1,19 @@
+import socket
+j =0
+z =0
+port = input("please input your port for finde local ip: ")
+for i in range(256):
+   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   ip = f'127.{z}.{j}.{i}'
+   result = sock.connect_ex((ip,8090))
+   if i == 255:
+       i = 1
+       j+=1
+   if j==255:
+       j = 1
+       z+=1
+   if result == 0:
+      print(ip +" Port is open")
+   else:
+      print(ip +" Port is not open")
+   sock.close()
